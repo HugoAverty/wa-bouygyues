a08b1925bbe15117bc99cd5d1799a550637098a6
Hugo Averty <hugoaverty@gmail.com>
2022-08-04 19:04:18 +0200

    Fixing CI

--- src/airport.ts
+++ src/airport.ts
@@ -10,10 +10,11 @@ let popBar: any = undefined;
 let popNFT: any = undefined;
 let popGate: any = undefined;
 
+let urlTropical: any = undefined;
+
 // Waiting for the API to be ready
 WA.onInit().then(() => {
 
-    console.log('Hello world !');
 
     WA.room.onEnterLayer('zoneComptoir1').subscribe(() => {
         popComptoir1 = WA.ui.openPopup("comptoir1Popup","Hello, you are rather tropical climate or Caribbean climate ?",
@@ -21,32 +22,19 @@ WA.onInit().then(() => {
                 label: "Tropical",
                 className: "primary",
                 callback: () => {
-                    WA.room.website.create({
-                      name: "my_website",
-                      url: "https://example.com",
-                      position: {
-                        x: 64,
-                        y: 128,
-                        width: 320,
-                        height: 240,
-                      },
-                      visible: true,
-                      allowApi: true,
-                      allow: "fullscreen",
-                      origin: "map",
-                      scale: 1,
-                    });
+                    urlTropical = WA.nav.openCoWebSite('https://player.vimeo.com/video/680787827');
+                    popComptoir1.close();
                 }
             },
             {
                 label: "Caribbean",
-                className: "secondary",
+                className: "success",
                 callback: () => {
-                    window.open("https://vimeo.com/684568939/71107d1cc5", '_blank');
+                    WA.nav.openCoWebSite('https://player.vimeo.com/video/684568939/71107d1cc5');
+                    popComptoir1.close();
                 }
             }]
         );
-        console.log('zoneComptoir1');
     })
     WA.room.onEnterLayer('zoneComptoir2').subscribe(() => {
         popComptoir2 = WA.ui.openPopup("comptoir2Popup","Good morning, you are a beach or culture person?",[
@@ -54,45 +42,42 @@ WA.onInit().then(() => {
                 label: "Beach",
                 className: "primary",
                 callback: () => {
-                    window.open("https://vimeo.com/manage/videos/554125018/eed15f29a1", '_blank').focus();
+                    WA.nav.openCoWebSite('https://vimeo.com/manage/videos/554125018/eed15f29a1');
                 }
             },
             {
                 label: "Culture",
-                className: "secondary",
+                className: "success",
                 callback: () => {
-                    window.open("https://vimeo.com/435014076", '_blank').focus();
+                    WA.nav.openCoWebSite('https://vimeo.com/435014076');
                 }
             }
-            ]);
-        console.log('zoneComptoir2');
-    })
+            ]);    })
     WA.room.onEnterLayer('zoneBar').subscribe(() => {
         popBar = WA.ui.openPopup("barPopup","You want to get high before you get in the air?",[
             {
                 label: "NO",
                 className: "primary",
                 callback: () => {
-                    window.open("https://www.youtube.com/watch?v=5YbjzztYbUo", '_blank').focus();
+                    WA.nav.openCoWebSite('https://www.youtube.com/watch?v=5YbjzztYbUo');
                 }
             },
             {
                 label: "YES",
-                className: "secondary",
+                className: "success",
                 callback: () => {
-                    window.open("", '_blank').focus();
+                    WA.nav.openCoWebSite('https://www.youtube.com/watch?v=5YbjzztYbUo');
                 }
             }
         ]);
-        console.log('zoneBar');
     })
     WA.room.onEnterLayer('zoneGate').subscribe(() => {
         popGate = WA.ui.openPopup("gatePopup","You want to know the secret life of your suitcase? We explain it all here!",[
             {
-                label: "NO",
+                label: "Watch",
                 className: "primary",
                 callback: () => {
-                    window.open("https://vimeo.com/452151588", '_blank');
+                    WA.nav.openCoWebSite('https://vimeo.com/452151588');
                 }
             }
         ]);
@@ -119,6 +104,8 @@ function closePopUp(){
     if (popComptoir1 !== undefined) {
         popComptoir1.close();
         popComptoir1 = undefined;
+
+        const coWebsites = WA.nav.getCowebSites();
     }
     if (popComptoir2 !== undefined) {
         popComptoir2.close();
